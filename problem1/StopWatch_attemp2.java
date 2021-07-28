6f70c63e593df67317f5bbd9e14746d5aec3cd28
Arindam Chatterjee <ari30feb@gmail.com>
2021-07-28 11:39:11 +0530

    Keeping only three implementations of State

---
+++ problem1/StopWatch_attemp2.java
@@ -0,0 +1,77 @@
+package problem1;
+
+public interface State{
+	public boolean start()
+	{
+		return false;
+	}
+	public boolean stop()
+	{
+		return false;
+	}
+	public boolean pause()
+	{
+		return false;
+	}
+	public boolean resume()
+	{
+		return false;
+	}
+}
+
+public class RunningState implements State {
+	public boolean start()
+	{
+		return true;
+	}
+	public boolean resume()
+	{
+		return true;
+	}
+}
+public class StopState implements State {
+	public boolean stop()
+	{
+		return true;
+	}
+}
+public class PauseState implements State {
+	public boolean pause()
+	{
+		return true;
+	}
+}
+
+
+
+
+public class StopWatch {
+	
+	State s;
+	
+	public void changeState(State newState)
+	{
+		s = newState;
+	}
+	
+	public boolean start()
+	{
+		
+		return s.start();
+	}
+	public boolean stop()
+	{
+		
+		return s.stop();
+	}
+	public boolean pause()
+	{
+		
+		return s.pause();
+	}
+	public boolean resume()
+	{
+		
+		return s.resume();
+	}
+}
